584478f1413cbad01c277324e3ca97ea35f854da
ME_Kun_Han <hanvskun@hotmail.com>
2020-04-08 00:46:37 +0800

    72

--- ans/src/main.rs
+++ ans/src/main.rs
@@ -1108,6 +1108,87 @@ fn test_in_path_sum() {
     assert_eq!(min_path_sum(grid), 7);
 }
 
+/**
+ * 72. Edit Distance
+ * Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+ * You have the following 3 operations permitted on a word:
+ * 
+ * Insert a character
+ * Delete a character
+ * Replace a character
+ * Example 1:
+ * 
+ * Input: word1 = "horse", word2 = "ros"
+ * Output: 3
+ * Explanation: 
+ * horse -> rorse (replace 'h' with 'r')
+ * rorse -> rose (remove 'r')
+ * rose -> ros (remove 'e')
+ * Example 2:
+ * 
+ * Input: word1 = "intention", word2 = "execution"
+ * Output: 5
+ * Explanation: 
+ * intention -> inention (remove 't')
+ * inention -> enention (replace 'i' with 'e')
+ * enention -> exention (replace 'n' with 'x')
+ * exention -> exection (replace 'n' with 'c')
+ * exection -> execution (insert 'u')
+ **/
+
+fn min_distance(word1: String, word2: String) -> i32 {
+    let mut n = word1.len();
+    let mut m = word2.len();
+
+    let mut steps = vec![vec![0; n+1]; m+1];
+
+    for i in 0..m+1 {
+        steps[i][0] = i;
+    }
+
+    for i in 0..n+1 {
+        steps[0][i] = i;
+    }
+
+    for i in 0..m+1 {
+        println!("{:?}", steps[i]);
+    }
+    println!("");
+    println!("");
+
+    let b1 = word1.as_bytes();
+    let b2 = word2.as_bytes();
+
+    for i in 0..m {
+        for j in 0..n {
+            if b1[j] == b2[i] {
+                steps[i+1][j+1] = steps[i][j];
+            } else {
+                steps[i+1][j+1] = std::cmp::min(std::cmp::min(steps[i][j], steps[i+1][j]), steps[i][j+1]) + 1;
+            }
+
+        }
+
+        for i in 0..m+1 {
+            println!("{:?}", steps[i]);
+        }
+
+        println!("");
+    }
+
+
+    return steps[m][n] as i32;    
+}
+
+#[test]
+fn test_min_distance() {
+    let word1 = String::from("horse");
+    let word2 = String::from("ros");
+
+    assert_eq!(min_distance(word1, word2 ), 3);
+}
+
 fn main() {
     println!("the answer of leetcode.com using rust");
 }
