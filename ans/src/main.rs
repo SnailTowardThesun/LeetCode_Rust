753082da8d8f5f3b9952ebfaafc7dcc7ef4ed3eb
ME_Kun_Han <hanvskun@hotmail.com>
2021-08-21 22:17:04 +0800

    add 1796

--- ans/src/main.rs
+++ ans/src/main.rs
@@ -1656,7 +1656,27 @@ fn test_is_unival_tree() {
     print!("{}", ret)
 }
 
-/**
+/*
+ * 1796. Second Largest Digit in a String
+ */
+fn second_highest(s: String) -> i32 {
+    let mut ar = [false; 10];
+
+    s.as_bytes()
+        .iter()
+        .filter(|x| x.is_ascii_digit())
+        .map(|x| x - b'0')
+        .for_each(|x| ar[x as usize] = true);
+
+    ar.iter()
+        .enumerate()
+        .filter_map(|(ind, val)| if *val { Some(ind as i32) } else { None })
+        .rev()
+        .nth(1)
+        .unwrap_or(-1)
+}
+
+/*
  * 1662. check if two string arrays are equivalent
  */
 fn array_strings_are_equal(word1: Vec<String>, word2: Vec<String>) -> bool {
