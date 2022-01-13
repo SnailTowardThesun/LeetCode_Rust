3e384b18d846c616b1a2c822dad49bc5247e18b8
ME_Kun_Han <hanvskun@hotmail.com>
2022-01-14 01:12:12 +0800

    add 373

--- ans/src/main.rs
+++ ans/src/main.rs
@@ -1668,6 +1668,59 @@ fn test_increasing_triplet() {
     assert_eq!(ret, true);
 }
 
+/**
+* 373.
+*/
+fn  k_smallest_pairs(nums1: Vec<i32>, nums2: Vec<i32>, k: i32) -> Vec<Vec<i32>> {
+    let mut sums_pair:Vec<Vec<i32>> = Vec::new();
+    let (len1, len2) = (nums1.len(), nums2.len());
+
+    if len1 == 0 || len2 == 0 {
+        return sums_pair;
+    }
+
+    let mut next = vec![0; len1];
+
+    let mut floor = 0;
+    let mut ceiling = 0;
+    let mut curr = 0;
+
+    for n in 0..k {
+        sums_pair.push(vec!(nums1[curr], nums2[next[curr]]));
+        next[curr] += 1;
+        if next[curr] >= len2 {
+            floor = curr +1;
+            if floor >= len1 {
+                break;
+            }
+        }
+        if curr == ceiling && ceiling +1 < len1{
+            ceiling += 1;
+        }
+        let mut sum = i32::MAX;
+        for c in floor..ceiling+1 {
+            if nums1[c] + nums2[next[c]] < sum{
+                sum = nums1[c] + nums2[next[c]];
+                curr = c;
+            }
+        }
+    }
+
+    return sums_pair;
+}
+
+#[test]
+fn test_k_smallest_pairs() {
+    let nums1 = vec![1, 2, 4];
+    let nums2 = vec![-1, 1, 2];
+    let ret = k_smallest_pairs(nums1, nums2, 10);
+    let mut ret = ret;
+    for i in 0..ret.len() {
+        print!("[{}, {}]\n", ret[i][0], ret[i][1]);
+    }
+
+}
+
 /*
  * 414. third max number
  */
